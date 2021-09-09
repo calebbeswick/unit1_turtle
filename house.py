9677a8d2fd4b0e699caca419739a86fd3e3ada1e
calebbeswick <beswica@ssfs.org>
2021-09-09 11:24:17 -0400

    created function to draw houses

--- house.py
+++ house.py
@@ -1,12 +1,32 @@
 import turtle
 
+
+turtle.penup()
 turtle.color("red")
-turtle.begin_fill()
-for x in range(3):
-    turtle.fd(100)
-    turtle.left(120)
-for y in range(4):
-    turtle.fd(100)
-    turtle.right(90)
-turtle.end_fill()
+turtle.goto(-100, 0)
+turtle.pendown()
+def draw_house():
+    turtle.begin_fill()
+    for x in range(3):
+        turtle.fd(50)
+        turtle.left(120)
+    for y in range(4):
+        turtle.fd(50)
+        turtle.right(90)
+    turtle.end_fill()
+
+def pen_up_house():
+    turtle.pendown()
+    draw_house()
+    turtle.penup()
+
+
+pen_up_house()
+turtle.goto(0, 0)
+pen_up_house()
+turtle.goto(100, 0)
+pen_up_house()
+
+
+
 turtle.exitonclick()
