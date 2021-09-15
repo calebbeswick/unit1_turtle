7aeda95eacd08a78d9cf19e7d80aa7cac7abed08
calebbeswick <beswica@ssfs.org>
2021-09-15 08:42:04 -0400

    made mercedes logo

--- mercedes.py
+++ mercedes.py
@@ -1,29 +1,20 @@
 import turtle
 
+turtle.pensize(3)
+turtle.color("silver")
 turtle.hideturtle()
-turtle.clear()
 turtle.circle(100)
-
 turtle.left(90)
 turtle.penup()
-turtle.forward(100)
+turtle.fd(100)
 turtle.pendown()
 
-turtle.forward(100)
+for x in range(3):
+    turtle.fd(100)
+    turtle.penup()
+    turtle.fd(-100)
+    turtle.left(120)
+    turtle.pendown()
 
-turtle.penup()
-turtle.backward(100)
-turtle.left(120)
-turtle.pendown()
-
-turtle.forward(100)
-turtle.penup()
-turtle.backward(100)
-turtle.left(120)
-turtle.pendown()
-turtle.forward(100)
-turtle.penup()
-turtle.backward(100)
-turtle.left(120)
 
 turtle.exitonclick()
